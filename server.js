6dde8e316d68ef7c0749eb3f25d6885039a1ba4e
Ujjawal kumar <ujjawal.kumar0619@gmail.com>
2021-06-15 17:20:35 +0530

    deploying version 1.0.0

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/turnover', require('./routes/turnover'));
 // Serve static(react) assets in production
 if (process.env.NODE_ENV === 'porduction') {
   // Set static folder
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
